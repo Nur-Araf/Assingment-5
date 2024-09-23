47929a0fdd8ba5de1565a4e1a8f241857d1850a0
Nur-Araf <cocshishir@gmail.com>
2024-09-23 15:56:11 +0600

    History Section

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ donateBtn.classList.add("selected");
 donateBtn.classList.remove("not-selected");
 historyBtn.classList.add("not-selected");
 
-donateBtn.addEventListener("click", () => {
+donateBtn.addEventListener("click", function () {
   donateSection.classList.remove("hidden");
   historySection.classList.add("hidden");
 
@@ -21,7 +21,7 @@ donateBtn.addEventListener("click", () => {
   historyBtn.classList.add("not-selected");
 });
 
-historyBtn.addEventListener("click", () => {
+historyBtn.addEventListener("click", function () {
   donateSection.classList.add("hidden");
   historySection.classList.remove("hidden");
 
@@ -41,7 +41,6 @@ let totalBalance = parseInt(mainBalance);
 let noakhaiBalance = parseInt(noakBalance);
 let feniBalance = parseInt(fenBalance);
 let aidBalance = parseInt(aiBalance);
-
 
 function validAmount(inputId) {
   const input = document.getElementById(inputId).value;
@@ -69,10 +68,27 @@ function updateBalances(donationAmount, balanceId) {
   balanceElement.innerText = balance;
 }
 
+///------ History -----///
+
+function addHistory(donationAmount, placeId) {
+  const heading = document.getElementById(placeId).innerText;
+  let date = new Date();
+
+  const newElement = document.createElement("div");
+  newElement.innerHTML = `<div class="border-[1px] p-8 rounded-2xl mb-6">
+            <h1 class="font-bold text-xl pb-4">${donationAmount} ${heading}h</h1>
+            <p class="text-gray-700 font-light text-base">Date: ${date}</p>
+        </div>`;
+
+  historySection.appendChild(newElement);
+}
+
+// ------ Update the Balances and History ----- //
 function noakhaliUpdateBalances() {
   const donationAmount = validAmount("noakhaliInput");
   if (donationAmount) {
     updateBalances(donationAmount, "noakhaiBalance");
+    addHistory(donationAmount, "noakhaliHeading");
   }
   document.getElementById("noakhaliInput").value = "";
 }
@@ -81,6 +97,7 @@ function feniUpdateBalances() {
   const donationAmount = validAmount("feniInput");
   if (donationAmount) {
     updateBalances(donationAmount, "feniBalance");
+    addHistory(donationAmount, "feniHeading");
   }
   document.getElementById("feniInput").value = "";
 }
@@ -89,80 +106,15 @@ function aidUpdateBalances() {
   const donationAmount = validAmount("aidInput");
   if (donationAmount) {
     updateBalances(donationAmount, "aidBalance");
+    addHistory(donationAmount, "aidHeading");
   }
   document.getElementById("aidInput").value = "";
 }
 
+
+// ------ Add Event Listener ----- //
 document
   .getElementById("btn")
   .addEventListener("click", noakhaliUpdateBalances);
 document.getElementById("btn-1").addEventListener("click", feniUpdateBalances);
 document.getElementById("btn-2").addEventListener("click", aidUpdateBalances);
-
-
-
-// function noaupdateBalances() {
-//   const noakhaliInput = document.getElementById("noakhaliInput").value;
-//   const noakhaliDonate = parseInt(noakhaliInput);
-
-//   if (isNaN(noakhaliDonate) || noakhaliDonate <= 0) {
-//     alert("Please enter a valid donation amount.");
-//     return;
-//   }
-  
-//    if(totalBalance < noakhaliDonate) {
-//      alert("Insufficient Balance");
-//      return;
-//    }
-//   totalBalance -= noakhaliDonate;
-//   noakhaiBalance += noakhaliDonate;
-
-//   document.getElementById("balance").innerText = totalBalance;
-//   document.getElementById("noakhaiBalance").innerText = noakhaiBalance;
-// }
-
-// document.getElementById("btn").addEventListener("click", noaupdateBalances);
-
-// function fenupdateBalances() {
-//   const feniInput = document.getElementById("feniInput").value;
-//   const feniDonate = parseInt(feniInput);
-
-//   if (isNaN(feniDonate) || feniDonate <= 0) {
-//     alert("Please enter a valid donation amount.");
-//     return;
-//   }
-
-//   if (totalBalance < feniDonate) {
-//     alert("Insufficient Balance");
-//     return;
-//   }
-//   totalBalance -= feniDonate;
-//   feniBalance += feniDonate;
-
-//   document.getElementById("balance").innerText = totalBalance;
-//   document.getElementById("feniBalance").innerText = feniBalance;
-// }
-
-// document.getElementById("btn-1").addEventListener("click", fenupdateBalances);
-
-// function aidupdateBalances() {
-//   const aidInput = document.getElementById("aidInput").value;
-//   const aidDonate = parseInt(aidInput);
-
-//   if (isNaN(aidDonate) || aidDonate <= 0) {
-//     alert("Please enter a valid donation amount.");
-//     return;
-//   }
-
-//   if (totalBalance < aidDonate) {
-//     alert("Insufficient Balance");
-//     return;
-//   }
-//   totalBalance -= aidDonate;
-//   aidBalance += aidDonate;
-
-//   document.getElementById("balance").innerText = totalBalance;
-//   document.getElementById("aidBalance").innerText = aidBalance;
-// }
-
-// document.getElementById("btn-2").addEventListener("click", aidupdateBalances);
